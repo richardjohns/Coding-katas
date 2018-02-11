bcafa0b4fc632b5a57499dfb2ea5518b0eb3bd1d
richardjohns <richard.johns193@gmail.com>
2018-02-11 23:04:30 +1300

    text updates

--- ruby.js
+++ ruby.js
@@ -206,3 +206,53 @@ movie_ratings = {
 # Add your code below!
 
 movie_ratings.each_key { |title| puts title }
+
+# MOVIE CRUD EXAMPLE
+
+movies = {
+  StarWars: 4.8, 
+  Divergent: 4.7
+  }
+
+puts "What would you like to do? "
+
+choice = gets.chomp
+
+case choice
+when "add"
+  puts "What movie would you like to add? "
+  title = gets.chomp
+  if movies[title.to_sym].nil? 
+    puts "What rating does the movie have? "
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  else
+    puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+  end
+when "update"
+  puts "What movie would you like to update? "
+  title = gets.chomp
+  if movies[title.to_sym].nil? 
+    puts "That movie does not exist."
+  else
+    puts "What is the new rating? "
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  end
+when "display"
+  movies.each do |title, rating| 
+  puts "#{title}: #{rating}"
+  end
+when "delete"
+   puts "What movie would you like to delete? "
+   title = gets.chomp
+   if movies[title.to_sym].nil?
+     puts "movie not found"
+   else
+     movies.delete(title)
+     puts "movie deleted!"
+     
+   end
+else
+  puts "Error!"
+end
