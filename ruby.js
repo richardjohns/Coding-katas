762e64af5da0d8c83b9e734c55f50c147c8f8cd9
richardjohns <richard.johns193@gmail.com>
2018-02-12 10:33:29 +1300

    recent text changes

--- ruby.js
+++ ruby.js
@@ -256,3 +256,18 @@ when "delete"
 else
   puts "Error!"
 end
+
+# One line case statements
+
+puts "Hello there!"
+greeting = gets.chomp
+
+# Add your case statement below!
+
+case greeting
+  when "English" then puts "Hello!"
+  when "French" then puts "Bonjour!"
+  when "German" then puts "Guten Tag!"
+  when "Finnish" then puts "Haloo!"
+  else puts "I don't know that language!"
+end
