86ddaa4126be42b05b78110180bea134f9323826
richardjohns <richard.johns193@gmail.com>
2018-02-22 23:46:14 +1300

    js memory game mostly done

--- app.js
+++ app.js
@@ -571,65 +571,65 @@
 
 // First cut of flattenner using for loops - non-recursive ie doesn't work for infinite nested arrays.
 
-function flattenerOld(arr) {
-    newArr2 = []
-    
-    for (i = 0; i < arr.length; i++) {
-        // loop over items in an array...
-        // for each nested array found, loop over that nested array checking for more nested arrays and push any plain numbers found to newArr.
-        
-        // set test variables for each layer of the array.
-        // use 'isArray' test so that conditions aren't triggered by key/value objects.
-        const firstLayerObjFound = Array.isArray(arr[i])        
-
-        if (firstLayerObjFound) {
-            for (j = 0; j < arr[i].length; j++) {
-    
-              const secondLayerObjFound = Array.isArray(arr[i][j])
-              if (secondLayerObjFound) {
-                    for (k = 0; k < arr[i][j].length; k++) {
-                    newArr2.push(arr[i][j][k])
-                    }
-                } else {
-                newArr2.push(arr[i][j])
-                }
-            }
-        } else {
-            newArr2.push(arr[i])
-        }
-    }
-
-    console.log("This is newArr2: ", newArr2)
-}
-
-flattenerOld([[1,2,[3]],4,[5,6],[7,[8,[9,10]]]])
+//function flattenerOld(arr) {
+//    newArr2 = []
+//    
+//    for (i = 0; i < arr.length; i++) {
+//        // loop over items in an array...
+//        // for each nested array found, loop over that nested array checking for more nested arrays and push any plain numbers found to newArr.
+//        
+//        // set test variables for each layer of the array.
+//        // use 'isArray' test so that conditions aren't triggered by key/value objects.
+//        const firstLayerObjFound = Array.isArray(arr[i])        
+//
+//        if (firstLayerObjFound) {
+//            for (j = 0; j < arr[i].length; j++) {
+//    
+//              const secondLayerObjFound = Array.isArray(arr[i][j])
+//              if (secondLayerObjFound) {
+//                    for (k = 0; k < arr[i][j].length; k++) {
+//                    newArr2.push(arr[i][j][k])
+//                    }
+//                } else {
+//                newArr2.push(arr[i][j])
+//                }
+//            }
+//        } else {
+//            newArr2.push(arr[i])
+//        }
+//    }
+//
+//    console.log("This is newArr2: ", newArr2)
+//}
+//
+//flattenerOld([[1,2,[3]],4,[5,6],[7,[8,[9,10]]]])
 
 // Refactored test with functional programming and recursive function structure.
 
-function flattener(arr) {
-    outputArray = []
-    
-    function checkAndPush(element) {
-        
-        const isItemArray = item => Array.isArray(item)
-        const pushToOutputArray = item => outputArray.push(item)
-            
-        function pushNestedArray(element) {
-            element.forEach((item) => {
-                isItemArray(item) ? pushNestedArray(item) : pushToOutputArray(item)
-            })
-        }
-        
-        isItemArray(element) ? pushNestedArray(element) : pushToOutputArray(element)
-    }
-    
-    arr.forEach((element) => checkAndPush(element))
-
-    console.log("This is outputArray: ", outputArray)
-}
-
-// testing edge cases of multiple arrays nested 3 layers deep.
-flattener([[1,2,[3]],4,[5,6],[7,[8,[9,10]]]])
+//function flattener(arr) {
+//    outputArray = []
+//    
+//    function checkAndPush(element) {
+//        
+//        const isItemArray = item => Array.isArray(item)
+//        const pushToOutputArray = item => outputArray.push(item)
+//            
+//        function pushNestedArray(element) {
+//            element.forEach((item) => {
+//                isItemArray(item) ? pushNestedArray(item) : pushToOutputArray(item)
+//            })
+//        }
+//        
+//        isItemArray(element) ? pushNestedArray(element) : pushToOutputArray(element)
+//    }
+//    
+//    arr.forEach((element) => checkAndPush(element))
+//
+//    console.log("This is outputArray: ", outputArray)
+//}
+//
+//// testing edge cases of multiple arrays nested 3 layers deep.
+//flattener([[1,2,[3]],4,[5,6],[7,[8,[9,10]]]])
 
 // BACKUP OF FLICK TEST
 
@@ -661,219 +661,8 @@ flattener([[1,2,[3]],4,[5,6],[7,[8,[9,10]]]])
 //
 //flattener([[1,2,[3]],4,[5,6],[7,[8,[9,10]]]])
 
-// Codewars - test if number is a perfect square. Code asks if number ends in 1, 4, 5, 6, or 9 - if so is perfect square. Code works but theory is wrong eg 26 is not a perfect square - don't believe everything you read on the internet!
 
-var isSquare = function(n){
- 
-  function squareCheck(n) {
-     // All perfect squares end in 1, 4, 5, 6, 9 or 00
-     var stringDigits = (""+n).split("");
-     var digits = stringDigits.map(str => Number(str))
-     var lastDigit = digits[digits.length - 1]
-     var scndLastDigit = digits[digits.length - 2]
-     
-     var testArr = []
-     testArr.push(scndLastDigit, lastDigit)
-     console.log('This is testArr: ',testArr)
-     
-     if (testArr[0] == 0 && testArr[1] == 0) {
-//        console.log('This was a square with double-zero')
-       return true
-     } else if (testArr[0] == undefined || 0 && testArr[1] == 1 || 4 || 5 || 6 || 9) {
-//        console.log('This was a square')
-       return true
-     } else {
-       return false
-     }
-  }
 
-  if (n < 0) {
-    console.log("Negative numbers cannot be square numbers")
-    return false
-  } else if (n > 0) {
-    squareCheck(n)  
-  }
- }
 
-// Codewars -- accum("abcd"); becomes "A-Bb-Ccc-Dddd". Mostly there except upper/lowercase. 
 
-function accum(s) {
-  var str = s.split("");
-  strCounter = 0
-  lenCounter = 1
-  var outputLetters = []
 
-  for (i = 0; i < str.length; i++) {
-  
-    for (j = 0; j < lenCounter; j++) {
-      var input = str[strCounter]
-      var inputLetter = input.toLowerCase()
-      
-      if (j > 0) {
-        inputLetter = input.toLowerCase()
-      } else {
-        inputLetter = input.toUpperCase()
-      }
-
-      if (j === lenCounter - 1) {
-        outputLetters.push(inputLetter)
-        outputLetters.push("-")
-      } else {
-        outputLetters.push(inputLetter)
-      }    
-    }
-
-    var outputStr = outputLetters.join("")
-    strCounter ++
-    lenCounter ++
-
-  }
-  console.log('This is outputStr: ',outputStr)  
-  outputStr = outputStr.slice(0,-1)
-  return outputStr
-}
-
-// Codewars - DNA strand. Each letter is paired, and each element of the string passed in is flipped to its pair before outputting
-
-function DNAStrand(dna){
-  return dna.split("").map((c,i) => c === "A" ? c = "T" 
-                                    : c === "T" ? c = "A"
-                                    : c === "G" ? c = "C"
-                                    : c === "C" ? c = "G"
-                                    : c
-  ).join("")
-}
-
-Test.assertEquals(DNAStrand("AAAA"),"TTTT","String AAAA is");
-
-// Codewars - Multiples of 3 or 5 below the number passed in
-
-function solution(number){
-  arr = []
-  for (i = 1; i < number; i++) {
-    arr.push(i)
-  }
-  
-  return arr.filter((a,i) => a % 3 === 0 || a % 5 === 0).reduce((a,b) => a + b, 0)
-}
-
-// Codewars - Take number, split into array of numbers, square each number and rejoin
-
-function squareDigits(num){
-  var digits = (""+num).split("")
-  var nums = digits.map(d => Number(d)).map(d => d * d).map(String).join("")
-  return Number(nums)
-}
-
-// Codewars - Take array of numbers, find minimum, delete out using splice.
-
-function removeSmallest(numbers) {
-    min = Math.min.apply(Math, numbers)
-    delIndex = numbers.indexOf(min)
-    numbers.splice(delIndex, 1)
-    return numbers
-  }
-
-Test.assertSimilar(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]");
-
-// Codewars - Playing with digits - Code working except for edge case involving passing number with 0 in it to function
-
-// Some numbers have funny properties. For example:
-// 89 --> 8¹ + 9² = 89 * 1
-// 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
-
-// 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
-
-// Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p we want to find a positive integer k, if it exists, 
-// such as the sum of the digits of n taken to the successive powers of p is equal to k * n. In other words:
-
-// Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
-
-// If it is the case we will return k, if not return -1.
-
-// Note: n, p will always be given as strictly positive integers.
-
-function digPow(n, p){
-  kArr = []
-  powArr = []
-  ansVal = 1
-  
-  var digits = (""+n).split("")
-  var digArr = digits.map(d => Number(d))
-  console.log('This is digArr: ',digArr)  
-  
-  for(i = 1; i < 129340; i++) {
-    kArr.push(n * i)
-  }
-  
-  for (i = 0; i < digArr.length; i++) {
-    powArr.push(Math.pow(digArr[i],p + i))
-  }
-  powArrSum = powArr.reduce((a,b) => a + b)
-  console.log('This is powArrSum: ',powArrSum)
-  
-  kArrMatch = kArr.some(r => r === powArrSum)
-  //kArrMatch = kArr.some(r=> powArr.includes(r)) - Nice - matches value in one array to any found in target array.
-  kArrFind = kArr.find(j => j == powArrSum)
-  kArrIndex = kArr.indexOf(kArrFind)
-  
-  console.log('This is kArr: ',kArr)
-  console.log('This is kArrMatch: ',kArrMatch)
-  console.log('This is KArrFind: ',kArrFind)
-  console.log('This is KArrIndex: ',kArrIndex)
-  console.log('This is powArr: ',powArr)
-
-  kArrMatch ? ansVal = kArrIndex + 1
-            : ansVal = -1 
-   
-  return ansVal
-}
-
-// Codewars - Title case - incomplete as doesn't yet deal with minorWords using .replace function
-
-function titleCase(title, minorWords) {
-  console.log('typeof title: ',typeof title)
-  console.log('title: ',title)
-  
-  if (title === '') {
-    return title
-  } else {
-  var titleLow = title.toLowerCase().split(" ")
-  console.log('titleLow: ',titleLow)
-  return titleLow.map(l => l.charAt(0).toUpperCase() + l.slice(1)).join(' ')
-  }
-}
-
-// Codewars - format an array of names like 'Bart, Lisa & Maggie'
-
-function list(names){
-  outputArr = []
-  names.forEach((name,i) => {
-      names.length === 1 ? outputArr.push(name.name)
-      : i === names.length - 1 ? outputArr.push('& ' + name.name)
-      : i === names.length - 2 ? outputArr.push(name.name + ' ')
-                               : outputArr.push(name.name + ', ')
-  })
- return outputArr.join('')
-}
-
-Test.assertEquals(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}]), 'Bart, Lisa, Maggie, Homer & Marge',
-"Must work with many names")
-
-// Codewars - Sort only the odd numbers of an array. Need to sort some edge cases... where same number in array, indexOf returns 1st index and not subsequent.
-
-function sortArray(array) {
-  var oddNumIdx = []
-  array.forEach(a => a % 2 !== 0 ? oddNumIdx.push(array.indexOf(a)) : a)
-  
-  var oddNumArr = array.filter(a => a % 2 !== 0).sort((a,b) => a - b)
-  
-  console.log('oddNumIdx: ',oddNumIdx)
-  console.log('oddNumArr: ',oddNumArr)
-
-  var newArr = array
-  oddNumArr.forEach((a,i) => newArr.splice(oddNumIdx[i],1,oddNumArr[i]))
-  
-  return newArr
-
-}
---
+++ game.js
@@ -0,0 +1,116 @@
+// MEMORY GAME - end game logic bit dicey still
+
+function memoryGame() {
+    
+console.log('Game running')
+var username = prompt("What is your name?");
+console.log(`Hi ${username}! 
+Welcome to the amazing memory pair game! 
+
+The rules are as follows: 
+1. You'll be presented with 20 boxes with contents hidden by an 'X'. Choose the number of a box to reveal its contents by typing in the boxes' number when prompted.
+2. Then once you've memorised the box's contents, choose another box to reveal it's contents and try to get a match!
+3. Once you get a match, the contents of that box will stay revealed. Keep trying to get a match until all boxes are revealed!
+`)
+
+xArr = []
+function genXArr() {
+    xBox = ['X']
+    for(i = 0; i < 20; i++) {
+        xArr.push([i,xBox])
+    }
+}
+
+ansArr = [
+    "blob",
+    "frag",
+    "shot",
+    "leg",
+    "mum!",
+    "dad!",
+    "bag",
+    "four",
+    "cheese",
+    "dad!",
+    "blob",
+    "frag",
+    "shot",
+    "leg",
+    "mum!",
+    "dad!",
+    "bag",
+    "four",
+    "cheese",
+    "dad!",
+]    
+
+round = 1
+
+function gameStart() {
+    genXArr()
+    console.log('Here are the boxes...')
+    console.log(JSON.stringify(xArr, null, ''))
+    boxChoice()
+}
+
+function nextRound() {
+    console.log('Here are the boxes...')
+    console.log(JSON.stringify(xArr, null, ''))
+    xCheck()
+}  
+    
+function endGame() {
+    console.log('You win! Lets play again')
+    xArr = []
+    gameStart()
+}
+
+function xCheck() {
+    round += 1
+    console.log(`This is round ${round}`)
+    var allMatched = xArr.every(a => {
+        // console.log('a[1]: ',a[1])
+        a[1] !== 'X' 
+    })
+    console.log('This is allMatched: ',allMatched)
+    allMatched ? endGame() : boxChoice()
+}    
+    
+function boxChoice() {
+    var firstBox = prompt("Great! Here are the boxes showing below... What box would you like to see first?")
+    xArr.splice(firstBox,1,[Number(firstBox), ansArr[firstBox]])
+
+    console.log('Et voila...')
+    console.log(JSON.stringify(xArr, null, ''))
+
+    var secondBox = prompt('Awesome, what\'s your second box?')
+    xArr.splice(secondBox,1,[Number(secondBox), ansArr[secondBox]])
+    console.log('Nice one')
+    console.log(JSON.stringify(xArr, null, ''))
+
+    console.log('This is xArr[0]: ',xArr[0])
+    pairCheck(firstBox, secondBox)
+}
+
+function pairCheck(firstBox, secondBox) {
+if (xArr[firstBox][1] === xArr[secondBox][1]) {
+    console.log(`You've matched ${xArr[firstBox][1]}!
+Your first choice ${firstBox}
+matches your second choice ${secondBox}
+Keep going!
+`)
+} else {
+    console.log(`Looks like it's not a match?
+Your first choice ${firstBox} - ${xArr[firstBox][1]}
+doesn't match your second choice ${secondBox} - ${xArr[secondBox][1]}
+Don't give up! Keep trying!
+`)
+}  
+nextRound()
+}
+
+gameStart()
+
+}
+
+memoryGame()
